2cdccb345172f3b313fae5dda563508d74b3a4b1
Ajin Sunny <ajinsunny@users.noreply.github.com>
2023-05-16 09:22:54 -0400

    Update TodoItems.js
    Binding in Constructor: You are binding your class methods in the constructor, which is a common practice in class components. However, it might be more convenient and cleaner to use arrow functions for methods in your class. This way, you don't need to bind this context in the constructor.
    
    Prop Types: It's a good practice to define propTypes for your components. This helps with validation and makes sure that the props you're expecting are being passed in correctly.
    
    Destructuring Props: In the render method, you can destructure the props for cleaner code.
    
    Use const instead of var: In modern JavaScript, const or let is preferred over var due to their block scope.
    
    Semantic HTML: Use button for delete action as it is more semantic, and it can help with accessibility.

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -24,3 +24,48 @@ class TodoItems extends Component {
 };
 
 export default TodoItems;
+
+
+// Here is the version that I would use 
+
+/*
+
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+class TodoItems extends Component {
+	createTasks = item => {
+		return (
+			<li key={item.key}>
+				{item.text}
+				<button onClick={() => this.delete(item.key)}>Delete</button>
+			</li>
+		)
+	}
+
+	delete = key => {
+		this.props.delete(key);
+	}
+
+	render() {
+		const { entries } = this.props;
+		const listItems = entries.map(this.createTasks);
+
+		return (
+			<ul className="theList">
+				{listItems}
+			</ul>
+		);
+	}
+};
+
+TodoItems.propTypes = {
+	entries: PropTypes.array.isRequired,
+	delete: PropTypes.func.isRequired,
+};
+
+export default TodoItems;
+
+
+
+*/
